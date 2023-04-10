0ce28d2430bc566bdfbefd812cac3ebc912d5fe5
JPratama7 <josepratama080@gmail.com>
2023-04-10 13:53:00 +0700

    add validator playground catcher

--- fiber/exc.go
+++ fiber/exc.go
@@ -2,6 +2,7 @@ package fiber
 
 import (
 	"fmt"
+	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -19,6 +20,16 @@ func ErrHandler(ctx *fiber.Ctx, err error) error {
 	if e, ok := err.(*fiber.Error); ok {
 		response.Code = e.Code
 		response.Status = e.Message
+	}
+
+	if e, ok := err.(validator.ValidationErrors); ok {
+		strFinal := "Error terjadi pada field :"
+		for _, v := range e {
+			strFinal += fmt.Sprintf(" %s", v.Field())
+		}
+		response.Code = fiber.StatusBadRequest
+		response.Status = strFinal
+
 	}
 
 	return response.WriteResponseBody(ctx)
