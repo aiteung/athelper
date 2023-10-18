5c3c763f0dd2e825af37ce502a775fa81772e240
JPratama7 <josepratama080@gmail.com>
2023-10-18 19:15:44 +0700

    feat(newresponsedata): new function untuk buat response data

--- fiber/response.go
+++ fiber/response.go
@@ -12,3 +12,12 @@ type ReturnData[T any] struct {
 func (rcv ReturnData[T]) WriteResponseBody(ctx *fiber.Ctx) error {
 	return ctx.Status(rcv.Code).JSON(rcv)
 }
+
+func NewReturnData[T any](code int, success bool, status string, data T) ReturnData[T] {
+	return ReturnData[T]{
+		Code:    code,
+		Success: success,
+		Status:  status,
+		Data:    data,
+	}
+}
